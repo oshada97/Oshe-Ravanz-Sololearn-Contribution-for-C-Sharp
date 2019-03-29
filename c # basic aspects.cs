5be0e35c3cd0b1d1feac42cbe87488dc9683b19d
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-03-29 23:53:41 +0530

    C sharp basic aspects.cs
    basic c sharp coding parts for simple application.
    Beginners must remember and practice/traain those codeing parts.

---
+++ c # basic aspects.cs
@@ -0,0 +1,17 @@
+//oshe ravanz contribution
+//basement of simple console programme
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace oshe
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
